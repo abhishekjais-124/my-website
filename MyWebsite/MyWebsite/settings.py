5b3776d5796dba0b0d9aff81b32b7ff738f6fe44
Abhishekjaiswal <abhishek.jaiswal@sliceit.com>
2022-06-02 11:10:45 +0530

    Implemented sending message through mail

--- MyWebsite/MyWebsite/settings.py
+++ MyWebsite/MyWebsite/settings.py
@@ -114,6 +114,23 @@ USE_I18N = True
 USE_TZ = True
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'abhishekjais124124@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'gyhepvxwwpznenya'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
--- MyWebsite/app/views.py
+++ MyWebsite/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -6,6 +7,18 @@ def home(request):
     return render(request,'app/index.html')
 
 def send_message(request):
-    print("Hello")
+    if request.method == 'POST':
+        subject = "Message from portfolio by " + request.POST.get('sname', 'Unknown')
+        message = request.POST.get('message', '')
+        email = 'abhishekjais124124@gmail.com'
+        print(subject,message,email)
+        send_mail(
+            subject,
+            message,
+            email,
+            ['abhishekjaiswal124@gmail.com'],
+            fail_silently=False,
+        )
+        print("sent")
     return render(request,'app/index.html')
     
